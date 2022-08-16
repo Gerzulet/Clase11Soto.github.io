d540622c30f814ec3527f47690e75213fa094c62
Gerzulet <german.alejandrozulet@gmail.com>
2022-08-15 21:17:11 -0300

    mitad de proyecto

--- db.js
+++ db.js
@@ -2,24 +2,241 @@
 
 let animacionesDB = `
 
-.inicio {
-  -webkit-animation: fadeinout 3s linear forwards;
-  animation: fadeinout 3s linear forwards;
-  opacity: 0;
+
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 1.5rem;
 }
 
-@-webkit-keyframes fadeinout {
-50% { opacity: 1; }
+.inputUsuario {
+	-webkit-animation: fade-in 3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fade-in 3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
 }
 
-@keyframes fadeinout {
-50% { opacity: 1; }
+
+
+@-webkit-keyframes fade-in {
+ 0% {
+   opacity: 0;
+ }
+ 100% {
+   opacity: 1;
+ }
+}
+@keyframes fade-in {
+ 0% {
+   opacity: 0;
+ }
+ 100% {
+   opacity: 1;
+ }
 }
 
-    
 
      
     `
 
 
+let inputInvalido = `
 
+#inputLog {
+	-webkit-animation: shake-horizontal 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+	        animation: shake-horizontal 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+}
+
+@-webkit-keyframes shake-horizontal {
+ 0%,
+ 100% {
+   -webkit-transform: translateX(0);
+           transform: translateX(0);
+ }
+ 10%,
+ 30%,
+ 50%,
+ 70% {
+   -webkit-transform: translateX(-10px);
+           transform: translateX(-10px);
+ }
+ 20%,
+ 40%,
+ 60% {
+   -webkit-transform: translateX(10px);
+           transform: translateX(10px);
+ }
+ 80% {
+   -webkit-transform: translateX(8px);
+           transform: translateX(8px);
+ }
+ 90% {
+   -webkit-transform: translateX(-8px);
+           transform: translateX(-8px);
+ }
+}
+@keyframes shake-horizontal {
+ 0%,
+ 100% {
+   -webkit-transform: translateX(0);
+           transform: translateX(0);
+ }
+ 10%,
+ 30%,
+ 50%,
+ 70% {
+   -webkit-transform: translateX(-10px);
+           transform: translateX(-10px);
+ }
+ 20%,
+ 40%,
+ 60% {
+   -webkit-transform: translateX(10px);
+           transform: translateX(10px);
+ }
+ 80% {
+   -webkit-transform: translateX(8px);
+           transform: translateX(8px);
+ }
+ 90% {
+   -webkit-transform: translateX(-8px);
+           transform: translateX(-8px);
+ }
+}
+
+`
+let loginDesvanecer = `#inputLog {
+	-webkit-animation: fade-out 1s ease-out both;
+	        animation: fade-out 1s ease-out both;
+}
+
+ @-webkit-keyframes fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+@keyframes fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+`
+
+let pagPrincipal = ` <div id="pagPrincipal" class="container  ">
+<div class="row my-4 ">
+  <div id="title" class="col border border-light text-light text-center">
+  </div>
+  <div class="col-1  mx-4 my-4">
+    <a href="#">
+      <img src="./Images/door-open.svg" alt="Logout" id="logout">
+
+    </a>
+  </div>
+</div>
+<div class="resumen row ">
+  <div class="col border border-light ">
+    <div class="row my-2 ">
+      <div class="col-3  mx-5 text-light">
+        Nombre
+      </div>
+      <div class="col-3  mx-4 text-light">
+        Fecha
+      </div>
+      <div class="col-2  mx-4 text-light">
+        Importancia
+      </div>
+      <div class="col-1  mx-4 text-light">
+        Flag
+      </div>
+
+
+      <div id="notas" class="row my-3 overflow-auto">
+        <div class="col-3  mx-5 text-light">
+         Clase de algebra
+        </div>
+        <div class="col-3 mx-4 text-light">
+          24/08/22
+        </div>
+        <div class="col-2  mx-4 text-light text-center">
+          Alta
+        </div>
+        <div class="col-1  mx-4 text-light text-center">
+          <a  href="#"><img class="bandera" src="./Images/flag.svg" alt=""></a>
+        </div>
+      </div>
+     
+    
+    </div>
+    
+    
+  
+  </div>
+  <div class="col-1 border border-light mx-4 fixed-right ">
+    <div class="col text-center my-5">
+    <a type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"><img class="bar" src="./Images/plus.svg" alt="Add"></a>
+    </div>
+    <div class="col text-center my-5">
+    <a href="#"><img class="bar" src="./Images/trash.svg" alt=""></a>
+    </div>
+    <div class="col text-center my-5 ">
+      <a href="#"><img class="bar" src="./Images/info.svg" alt=""></a>
+      </div>
+      <div class="col text-center my-5">
+      <a href="#"><img class="bar" src="./Images/play.svg" alt=""></a>
+      </div>
+      <div class="col text-center my-5">
+        <img class="cat" src="./Images/cat.png" alt="">
+        </div>
+    </div>
+     
+  </div>
+ 
+</div>
+
+
+<div class="offcanvas offcanvas-bottom text-center" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title text-light " id="offcanvasBottomLabel"> 
+    <button  type="button" class="btn-close mx-2" data-bs-dismiss="offcanvas" aria-label="Close">
+    <img class="bar" src="./Images/add.svg" alt="">
+    </button></h5>
+    <button  type="button" class="btn-close mx-2" data-bs-dismiss="offcanvas" aria-label="Close">
+    <img class="bar " src="./Images/trash.svg" alt="">
+    </button>
+  </div>
+  <div id="offcanvasBody" class="offcanvas-body small">
+  <div class="row my-2">
+    <div  class="form__group field col mx-1">
+      <input type="input" class="form__field" placeholder="Titulo" name="name" id="tituloNota" required />
+      <label for="name" class="form__label">Titulo</label>
+  </div> 
+  <div class="form__group field row mx-1">
+        <select class="form__field" id="validationDefault04" required>
+          <option selected disabled value="">Importancia</option>
+          <option>Alta</option>
+          <option>Media</option>
+          <option>Baja</option>
+          </select>
+  </div>
+  
+
+    <div class="mb-3">
+  <textarea  class="form__field" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  </div>
+</div>
+</div>
+
+ 
+</div>
+
+  
+
+`
--- main.js
+++ main.js
@@ -1,21 +1,89 @@
 const style = document.createElement("style"); 
 const body = document.getElementsByTagName("body")[0];
+style.innerHTML = `${animacionesDB} `; 
+document.head.appendChild(style);
 
+
+
+class notes {
+  constructor(nombre,fecha,importancia,flag) {
+    this.nombre = nombre;
+    this.fecha = fecha;
+    this.importancia = importancia;
+    this.flag = flag;
+    
+  }
+}
+
+log();
 
 function log (){
-  const inputUsuario = document.createElement("input"); 
-  style.innerHTML = `body {font-family: 'Exo 2', sans-serif; display: flex;justify-content: center; align-items: center;  background: #000; min-height: 100vh;} ${animacionesDB} `; 
-  document.head.appendChild(style);
-  body.innerHTML=`<h1 id="log" class="inicio text-light">Bienvenido</h1>`
   
-  setTimeout(function() {
-    body.innerHTML=`<h1 class="inicio text-light">Ingrese su usuario</h1>`
-    inputUsuario.classList.add("inputUsuario");
-    document.body.appendChild(inputUsuario);
+  body.innerHTML=`  <div id="inputLog" class="inputUsuario form__group field">
+  <input type="input" class="form__field" placeholder="Usuario" name="name" id="nombre" required />
+  <label for="name" class="form__label">Usuario</label>
+  </div> 
+  <button  onclick="validarInput()" type="button" class="inputUsuario my-4 btn btn-outline-light">Entrar</button>
+  ` 
 
-  }, 3500);
   
-  
+
+}
+
+
+
+
+function validarInput(){ 
+  const nombre = document.getElementById("nombre");  
+  nombre.value.length <= 1 ? style.innerHTML+=inputInvalido: validarInputLS(nombre.value);
+  setTimeout(() => {
+    style.innerHTML=`body {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      font-size: 1.5rem;
+    }`;
+  }, 900);
 
 
 }
+
+function validarInputLS(nombre){
+  var claves = Object.keys(localStorage); 
+  claves.some((key)=> key == nombre) ? main(nombre) : localStorage.setItem(nombre, []), main(nombre);
+
+} 
+
+function main(nombre){
+  style.innerHTML+=loginDesvanecer;
+  setTimeout(() => {
+    if (nombre[nombre.length-1]=="a"){
+      body.innerHTML=`  <h1 style="font-family: "'Courier New', Courier, monospace" class="text-light" id="bienvenida"> Bienvenida, ${nombre}. <h1>
+      ` } else {
+        body.innerHTML=`  <h1 style="font-family: "'Courier New', Courier, monospace" class="text-light" id="bienvenida"> Bienvenido, ${nombre}. <h1> `
+      }
+    }, 600);
+    
+    setTimeout(() => {
+      style.innerHTML = "";
+      body.innerHTML= pagPrincipal;
+      
+      const title = document.getElementById("title");
+    title.innerHTML = `  <h2 class="titulo my-4">¡Hola ${nombre}! Almacenaré tus notas.</h2>`
+    
+  }, 7000);
+
+  
+}
+
+
+/* TENEMOS QUE TENER EN MEMORIA UN USUARIO INVITADO ok
+SI EL USUARIO INGRESA UNA SOLO LETRA INVALIDAR INPUT ok
+CUANDO EL USUARIO INGRESA DOS PALABRAS INVALIDAR ok
+CUANDO EL USUARIO INGRESA UN VALOR CORRECTO CARGAR A LOCALSTORAGE ok
+
+
+
+*/
